b58f21f83dca44c4dc8fa3b3901b9ba5272f563b
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2022-05-26 08:08:14 +0700

    add welcome

--- src/main/java/com/kshrd/controller/HomeController.java
+++ src/main/java/com/kshrd/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.kshrd.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HomeController {
 
     @GetMapping("/")
     public String home(){
         return "Welcome home...!";
     }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome welcome...!";
+    }
+
+
 }
